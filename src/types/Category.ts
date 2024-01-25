9270957d2e8f5e6229410ca2ef3b4a2daa4f830a
Josh Buckland <josh.russell.buckland@gmail.com>
2024-01-25 21:38:41 +0000

    chore: update interesting stuff list

--- src/types/Category.ts
+++ src/types/Category.ts
@@ -1,8 +1,9 @@
 export const CATEGORY = {
-  COMPUTER_SCIENCE: 'Computer Science'
+  COMPUTER_SCIENCE: 'Computer Science',
+  GAME_DEVELOPMENT: 'Game Dev'
 } as const;
 
-export type Category = typeof CATEGORY[keyof typeof CATEGORY];
+export type Category = (typeof CATEGORY)[keyof typeof CATEGORY];
 
 export interface Item {
   name: string;
